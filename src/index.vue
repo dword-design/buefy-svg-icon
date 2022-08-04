56d073dad5dcf4ecb214033b0f0c7021b7225841
Sebastian Landwehr <sebastian.landwehr@honestly.com>
2022-08-04 14:25:36 +0200

    feat: init

---
+++ src/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <component :is="iconName" aria-hidden="true" />
+</template>
+
+<script>
+export default {
+  computed: {
+    iconName() {
+      return `${this.icon[1]
+        .replace(/-./g, x => x.toUpperCase()[1])
+        .replace(/\b\w/g, c => c.toUpperCase())}Icon`
+    },
+  },
+  props: ['icon', 'size'],
+}
+</script>
